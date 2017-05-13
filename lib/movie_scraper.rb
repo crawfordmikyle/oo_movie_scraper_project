1075ab3fd52346fdd9c68385d87ebf5ef2bd23a9
Mikyle Crawford <admin@MacBook-Pro.local>
2017-05-13 14:43:16 -0700

    Getting more working

--- lib/movie_scraper.rb
+++ lib/movie_scraper.rb
@@ -2,11 +2,12 @@ require "movie_scraper/version"
 require_relative "scraper"
 class Movie_Scraper
   def self.movie_list
-  	puts Scraper.data_arr
+  	data_array = Scraper.data_arr
+  	data_array.each {|entry| puts entry[:title]} 
   end
 
-  def movie_info
-  	puts "lorem"
+  def self.movie_info(title)
+  	data_array.each {|entry| puts entry[:rating]}
   end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,7 @@ class Scraper
 
 def self.data_arr
 @data_arr = [{title: "somthing1", rating: "5stars"},{title: "somthing2", rating: "5stars"},{title: "somthing3", rating: "5stars"}]
+@data_arr
 end
 
 end 
