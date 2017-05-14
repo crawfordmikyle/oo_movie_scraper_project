a1d266c7c79fa39e94e51c521ca36595587e1b0b
Mikyle Crawford <admin@MacBook-Pro.local>
2017-05-14 11:56:14 -0700

    Working on the cli again

---
+++ lib/main_methods.rb
@@ -0,0 +1,30 @@
+require "movie_scraper/version"
+require_relative "scraper"
+class Main_methods
+  def self.movie_list
+  	data_array = Scraper.data_arr
+  	data_array.each {|entry| puts entry[:title]} 
+  end
+
+  def self.movie_info(title)
+  	data_array.each {|entry| puts entry[:rating]}
+  end
+
+  def self.fandago_link_maker(city="Berkeley",state="CA")
+  	city = city
+  	state = state
+  	city_length =  city.split.size
+  	if city_length == 1
+  		@fandago_link = "http://www.fandango.com/#{city}_#{state}_movietimes"
+ 	elsif city_length == 2
+ 		@fandago_link = "http://www.fandango.com/#{city.split[0]}+#{city.split[1]}_#{state}_movietimes"
+  	elsif city_length == 3
+  		@fandago_link = "http://www.fandango.com/#{city.split[0]}+#{city.split[1]}+#{city.split[2]}_#{state}_movietimes"
+  	else 
+  		puts "Woops I cant find that city"
+  		exit!
+  	end 
+  end
+
+
+end
