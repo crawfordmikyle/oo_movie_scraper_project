f6da759bd3b7c863142515bfa78d2fd6466a5066
Mikyle Crawford <admin@MacBook-Pro.local>
2017-05-17 11:08:10 -0700

    Everything stored in each movie now

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,9 +1,10 @@
 class Movie
 attr_accessor :name, :reviews, :rating, :locations
 @@all = []
-def initialize(name,showtimes)
-	@locations_showtimes = {}
+def initialize(name,showtimes,location)
 	@name = name
+	@locations = {}
+	@locations[location] = showtimes
  	@@all << self
 end 
 
@@ -11,5 +12,8 @@ def self.all
 	@@all
 end 
 
-
+def add_location(locations_hash)
+	@locations.merge!(locations_hash)
 end 
+
+end
--- lib/movie_scraper.rb
+++ lib/movie_scraper.rb
@@ -10,5 +10,29 @@ class Movie_scraper
 	def initialize
 		@main_data_hash = Main_methods.get_movies
 		@main_data_hash.each_pair do |theater,movies_hash|
+			@location = [] << theater
+			@main_data_hash[theater].each_pair do |movie, showtimes|
+			@title = movie
+			@showtimes = showtimes[:showtimes]
+				if Movie.all.any? {|m|m.name == movie}
+					Movie.all.each do |m|
+						if m.name == movie 
+							location_hash = {@location => showtimes[:showtimes]}
+							m.add_location(location_hash)
+						end 
+					end 
+				else 	
+				movie = Movie.new(@title,@showtimes,@location)
+				end 
+			end 
+		end
+		list_results
+	end 
 
+	def list_results
+		Movie.all.each do |movie|
+			binding.pry
+			puts movie.name
+		end 
+	end 
 end
